0be13ef151846d5410625a406f76348b3f2fbcee
Oleksii Popov <oleksiipopov@admins-mbp.synapse.com>
2018-07-26 12:53:45 +0300

    modify app.js

--- app.js
+++ app.js
@@ -1,83 +1,14 @@
 
-window.onload = function () {
-    render();
-}
+import {model} from './model.js';
+import {removeAllChildren, createElement} from './helper.js';
+import {bindEvents} from './controller.js';
 
-let model = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 const todoForm = document.querySelector('#todo-form');
 const addInput = document.querySelector('#add-input');
 const todoList = document.querySelector('#todo-list');
 const todoItems = document.querySelectorAll('.todo-item');
-const message = document.querySelector('.todo-message');
 const todoMessage = document.querySelector('.todo-message');
-localStorage.setItem('items', JSON.stringify(model));
-todoForm.addEventListener('submit', addItemToDataBase);
-todoList.addEventListener('click', deleteItemFromDB);
-todoList.addEventListener('click', toggleTodoItem);
-todoList.addEventListener('click', updateItem)
 
-function addItemToDataBase (event) {
-    event.preventDefault();
-    if(!addInput.value) return alert("You need to add a task!");
-    let itemObject = {
-        ifChecked: '',
-        id: generateID(5),
-        title: addInput.value,
-        isEditing: false
-    }
-    model.push(itemObject);
-    addInput.value = '';
-    render(model);
-}
-
-function deleteItemFromDB (event) {
-    if(!event.target.classList.contains('delete')) return;
-    let ItemId = event.target.parentNode.getAttribute('data-id');
-    let ids = model.map(item=> {
-        return item.id;
-    })
-    let index = ids.indexOf(ItemId);
-    if(index != -1) {
-        model.splice(index, 1)
-    }
-    ids = model.map(item=> {
-        return item.id;
-    })
-    render(model);
-}
-
-function updateItem (event) {
-    if(!event.target.classList.contains('edit')) return;
-    let listItem = event.target.parentNode;
-    let textField = listItem.querySelector('.textfield');
-    let listItemId = listItem.getAttribute('data-id');
-    let ids = model.map(item=> {
-        return item.id;
-    });
-    let index = ids.indexOf(listItemId);
-    
-    if(model[index].isEditing) {
-        model[index].title = textField.value;
-    }
-    model[index].isEditing = !model[index].isEditing;
-    render(model);
-}
-
-function toggleTodoItem (event) {
-    if(!event.target.classList.contains('checkbox')) return;
-    let listItem = event.target.parentNode;
-    let listItemId = listItem.getAttribute('data-id');
-    let ids = model.map(item=> {
-        return item.id;
-    });
-    let index = ids.indexOf(listItemId);
-    if(!model[index].ifChecked) {
-        model[index].ifChecked = 'checked'
-    }else {
-        model[index].ifChecked = ''
-    }
-    render(model);
-}
 
 function render (initialModel) {
     let model = initialModel;
@@ -89,40 +20,7 @@ function render (initialModel) {
         todoMessage.classList.remove("hide");
     }
     localStorage.setItem('items', JSON.stringify(model));
-}
-
-function generateID(length) {
-    let text = ""
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
-    for(let i = 0; i < length; i++)  {
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-
-    return text
-}
-
-function removeAllChildren (rootElement) {
-    while(rootElement.firstChild) rootElement.removeChild(rootElement.firstChild)
-}
-
-function createElement(tag, props, ...children) {
-    const element = document.createElement(tag);
-    //we get only own properties
-    for (let prop in props) {
-        if (!props.hasOwnProperty(prop)) continue;
-        element[prop] = props[prop]; //element.type = props['type'];
-    }
-
-    if (children.length > 0) {
-        children.forEach(child => {
-            if (typeof child === 'string') {
-                child = document.createTextNode(child);
-            }
-            element.appendChild(child);
-        });
-    }
-    return element;
+    console.log(model)
 }
 
 function createListItem () {
@@ -154,10 +52,16 @@ function createListItem () {
         }, 'Delete');
 
         const listItem = createElement('li', {
-            className: checkbox.checked ? 'todo-item completed': model[i].isEditing ? 'todo-item editing': 'todo-item'
+            className: checkbox.checked ? 'todo-item completed' : model[i].isEditing ? 'todo-item editing': 'todo-item'
         }, checkbox, label, editInput, editButton, deleteButton);
         listItem.setAttribute('data-id', model[i].id)
         todoList.appendChild(listItem);
     }
 }
+
+render(model);
+bindEvents();
+
+export {render,todoForm, addInput, todoList, todoItems, todoMessage}
+
 
